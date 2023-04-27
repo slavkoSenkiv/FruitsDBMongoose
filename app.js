0ef97a6c64e65607a62376625dbababc57b1d0a9
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-27 10:02:17 +0300

    add mongoose code connection to app.js, try to make it work

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
+
+/* main().catch(err => console.log(err));
+async function main() {  await mongoose.connect('mongodb://localhost:27017/FruitsDB');  };
+ */
+mongoose.connect('mongodb://localhost:27017/FruitsDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+/* const fruitSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    rating: { type: Number, required: true },
+    review: { type: String, required: true }
+}); */
+const fruitSchema = new mongoose.Schema ({
+	name: String,
+	rating: Number,
+	review: String
+});
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const fruit = new Fruit ({
+    name: 'Apple1',
+    rating: 7,
+    review: 'pretty solid'
+});
+
+fruit.save();
+/* fruit.save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Data saved successfully');
+    }
+  }); */
