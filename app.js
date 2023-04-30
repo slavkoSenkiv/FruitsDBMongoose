fd1e27a1ee7608afc14f1966f23c05f22fb5219f
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-30 08:06:04 +0300

    try update and delete methods

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/FruitsDB');
 
-const fruitSchema = new mongoose.Schema ({
+mongoose.connect('mongodb://localhost/FruitsDB');
+/* const fruitSchema = new mongoose.Schema ({
 	name: String,
 	rating: Number,
 	review: String
+}); */
+
+
+const fruitSchema = new mongoose.Schema ({
+    name: {type: String, required: [true, 'name is required, you forgot to specify it']},
+    rating: {type: Number, min: 1, max: 10},
+    review: {type: String}
+});
+const personSchema = new mongoose.Schema ({
+    name: {type: String, required: [true, 'name is required, you forgot to specify it']},
+    age: {type: Number, min: 1, max: 100},
+    favoriteFruite: fruitSchema
 });
 
 const Fruit = mongoose.model('Fruit', fruitSchema);
 
-const apple = new Fruit ({
+const Person = mongoose.model('Person', personSchema);
+
+/* const apple = new Fruit ({
     name: 'Apple',
     rating: 1,
     review: 'solid'
@@ -24,18 +38,32 @@ const kiwi = new Fruit ({
     name: 'Kiwi',
     rating: 3,
     review: 'soft'
+}); 
+const fruitItem = new Fruit ({
+    rating: 3,
+    review: 'solid'
+});
+*/
+
+const fruit = new Fruit({
+    name: 'Pineapple',
+    rating: 3,
+    review: 'strange'
+});
+fruit.save();
+
+const person = new Person({
+    name: 'Amy',
+    age: 12,
+    favoriteFruite: fruit
 });
 
+person.save();
 //Fruit.insertMany([apple, orange, kiwi]);
-
-Fruit.find()
-    .then(function (fruits) {
-        fruits.forEach(function (fruit) {
-            console.log(fruit.name);
-        });
-        mongoose.connection.close();
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
+//fruitItem.save();
+//Fruit.find()
+//Fruit.updateOne({_id: '644cc525c3fbe00e1e6237c0'}, {name: 'Peach'});
+//const id = '644b52ef0ca459b3502808d1';
+//const ObjectId = new mongoose.Types.ObjectId;
+//Fruit.deleteOne({_id: '644b52ef0ca459b3502808d1'});
+//mongoose.connection.close();
